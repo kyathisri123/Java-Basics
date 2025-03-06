430f2cf314cb443c416c0e36a062c3619ce78277
kyathisri123 <kyathi2025@gmail.com>
2025-03-06 14:43:15 +0530

    Add files via upload

---
+++ DataTypesExample.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int myInt = 25;
+        boolean myBool = true;
+        char myChar = 'J';
+        float myFloat = 5.75f;
+        double myDouble = 99.99;
+
+        System.out.println("Integer: " + myInt);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("Character: " + myChar);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+    }
+}
+
