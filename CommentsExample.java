0a80be618291a04c4e7e1f82244593d4b7f7e568
kyathisri123 <kyathi2025@gmail.com>
2025-03-06 14:42:55 +0530

    Add files via upload

---
+++ CommentsExample.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+public class CommentsExample {
+    public static void main(String[] args) {
+        // This is a single-line comment
+
+        /*
+         * This is a multi-line comment.
+         */
+
+        /**
+         * This is a documentation comment.
+         */
+        System.out.println("Java Comments Example");
+    }
+}
+
