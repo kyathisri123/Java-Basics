e55c7fe36398f3d2f362aac55846aee35cb99b0c
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:10:42 +0530

    Add files via upload

---
+++ PrintNameFunction.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+public class PrintNameFunction {
+    public static void printName() {
+        System.out.println("Kyathi Sri");
+    }
+
+    public static void main(String[] args) {
+        printName();
+    }
+}
+
